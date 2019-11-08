e6f400c92bec3ef5b6d8807832b6d2153c69ae0a
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2019-11-08 03:37:26 +0100

    refactor(signup-controller):reduce logical complexity [Fixes #24)

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,52 +2,30 @@ const User = require('../database/models/userModel');
 const { generateToken } = require('../services/authService');
 
 exports.signUp = async (req, res) => {
-  const {
-    firstName,
-    lastName,
-    email,
-    password,
-    confirmPassword,
-    gender,
-    jobRole,
-    department,
-    address,
-  } = req.body;
+  const userData = req.body;
+  const isFieldMissing = () => Object.values(userData).filter((data) => data === '').length;
 
   try {
-    if (!(firstName
-        && lastName
-        && email
-        && password
-        && gender
-        && jobRole
-        && department
-        && address)) {
-      throw new Error('Missing required field(s)');
-    }
+    // check if user submitted data
+    if (isFieldMissing()) { throw new Error('Missing required field(s)'); }
 
     // create a service to clean the client data
 
     // check if user email already exists in the db
-    if (await User.emailExists(email)) { throw new Error('Email already exists'); }
+    if (await User.emailExists(userData.email)) { throw new Error('Email already exists'); }
 
     // check that name must be at least 2 characters
-    if (firstName.length < 2 || lastName.length < 2) { throw new Error('Firstname and lastname must contain at least 2 characters'); }
+    if (userData.firstName.length < 2 || userData.lastName.length < 2) { throw new Error('Firstname and lastname must contain at least 2 characters'); }
 
     // check if the user password and confirmPassword match
-    if (password !== confirmPassword) { throw new Error('Passwords don\'t match'); }
-    // check that gender is valid
-
-    // check that department is valid
-
-    // check that jobRole is valid
+    if (userData.password !== userData.confirmPassword) { throw new Error('Passwords don\'t match'); }
 
     // create the user
-    User.create(req.body)
+    User.create(userData)
       .then((obj) => {
         const {
-          firstname,
           id,
+          firstname,
           avatar,
           _timestamp,
         } = obj;
@@ -62,8 +40,8 @@ exports.signUp = async (req, res) => {
             token,
             userId: id,
             firstName: firstname,
-            avatar: avatar,
-            timestamp: _timestamp,
+            avatar,
+            _timestamp,
           },
         });
       });
