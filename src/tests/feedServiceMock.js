2d6d12e107436dd6bc5101358997e4178d5bf628
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2019-11-16 04:11:57 +0100

    refactor(coveralls):fix missed branch [Fixes #30]

---
+++ src/tests/feedServiceMock.js
@@ -0,0 +1,13 @@
+exports.feeds = [
+  { timestamp: 14 },
+  { timestamp: 16 },
+  { timestamp: 1 },
+  { timestamp: 2 },
+  { timestamp: 11 },
+  { timestamp: 2 },
+  { timestamp: 1 },
+  { timestamp: 4 },
+  { timestamp: 20 },
+  { timestamp: 1 },
+  { timestamp: 3 },
+];
--- src/utils/consoleLogger.js
+++ src/utils/consoleLogger.js
@@ -4,20 +4,20 @@ require('dotenv').config();
  * This logs errors into the console when the app's environment
  * is set to 'development'
  */
-class consoleLogger {
-  static log({ stack, message }) {
-    console.log('state');
-    // if (process.env.NODE_ENV === 'development') {
-    // eslint-disable-next-line no-console
-    // console.log('');
-    // console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
-    // console.log(message);
-    // console.log('--- Stack Trace ------------------------');
-    // console.log(stack);
-    // console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
-    // console.log('');
-    // }
-  }
-}
+// class consoleLogger {
+//   static log({ stack, message }) {
+//     console.log('state');
+//     // if (process.env.NODE_ENV === 'development') {
+//     // eslint-disable-next-line no-console
+//     // console.log('');
+//     // console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+//     // console.log(message);
+//     // console.log('--- Stack Trace ------------------------');
+//     // console.log(stack);
+//     // console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+//     // console.log('');
+//     // }
+//   }
+// }
 
 module.exports = consoleLogger;
