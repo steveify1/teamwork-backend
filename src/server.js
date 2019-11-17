bc1e1c30f512fbf48a6bdfe57b2550941b461559
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2019-11-17 06:35:53 +0100

    chore(release-v1):set app host - set host to 0.0.0.0 [Fixes #57]

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const runMigrations = require('./database/migrations');
 app.set('PORT', process.env.PORT || 3000);
 
 // Exposing the HTTP server object
-module.exports = app.listen(app.get('PORT'), '127.0.0.1', async () => {
+module.exports = app.listen(app.get('PORT'), '0.0.0.0', async () => {
   await runMigrations();
   console.log(`Server is up and running on port ${app.get('PORT')}`);
 });
