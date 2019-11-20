3ac830ed1bf18bba54431b2d70aed092a7fc644d
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2019-11-20 15:53:02 +0100

    chore(cors):implement cors middleware - write a middleware to set special http headers [Finishes #65]

--- src/tests/keyMapper.spec.js
+++ src/tests/keyMapper.spec.js
@@ -4,12 +4,12 @@ const data = require('./mockData/keyMapperMock');
 // Testing the key Mapper
 describe('keyMapper', () => {
   it('should reject the promise if the arrayOfObject argument is not an array', (done) => {
-    expectAsync(keyMapper(undefined, { name: 'steve' })).toBeRejected();
+    jasmine.expectAsync(keyMapper(undefined, { name: 'steve' })).toBeRejected();
     done();
   });
 
   it('should resolve the promise if the arrayOfObjects argument is really an array', (done) => {
-    expectAsync(keyMapper(data, { author_id: 'authorId' })).toBeResolved();
+    jasmine.expectAsync(keyMapper(data, { author_id: 'authorId' })).toBeResolved();
     done();
   });
 });
