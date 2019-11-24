c918f8feff09301851711a4d32e34b10a1338c51
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2019-11-24 02:55:18 +0100

    bug(cors):fix response headers [Fixes #69]

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -3,7 +3,7 @@
 */
 const cors = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, token, Token');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   next();
 };
