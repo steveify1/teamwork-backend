fe51bc60896ad36c9302ce057eea5725343bec69
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2019-11-28 11:43:21 +0100

    feature(feeds):update feed response - update feed to include basic information of the author [Finishes #71]

--- src/database/models/feedModel.js
+++ src/database/models/feedModel.js
@@ -8,12 +8,28 @@ class Feed extends Model {
 
   // returns article and gif posts in descending order
   async feeds() {
+    // const queryString = `
+    // SELECT a.id, a.title, a.article AS content, timestamp, author_id
+    // FROM articles AS a
+    // UNION
+    // SELECT g.id, g.title, g.image_url AS content, timestamp, author_id
+    // FROM gifs AS g
+    // INNER JOIN users
+    // ON author_id = users.id
+    // ORDER BY timestamp DESC
+    // LIMIT 20;
+    // `;
+
     const queryString = `
-    SELECT a.id, a.title, a.article AS content, timestamp, a.author_id
+    SELECT firstname, lastname, avatar, a.id, a.title, a.article AS content, timestamp, a.author_id
     FROM articles AS a
+    INNER JOIN users
+    ON a.author_id = users.id
     UNION
-    SELECT g.id, g.title, g.image_url AS content, timestamp, g.author_id
+    SELECT firstname, lastname, avatar, g.id, g.title, g.image_url AS content, timestamp, g.author_id
     FROM gifs AS g
+    INNER JOIN users
+    ON g.author_id = users.id
     ORDER BY timestamp DESC
     LIMIT 20;
     `;
